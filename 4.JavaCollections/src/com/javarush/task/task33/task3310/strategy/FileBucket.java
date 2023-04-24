19e9df9efb5482340c14915ef7604335fb7fa011
ArchPJ <washubrnfg85@gmail.com>
2023-04-24 23:04:11 +0300

    working on 3310 Shortener (9)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
@@ -8,8 +8,38 @@ import java.nio.file.Path;
 public class FileBucket {
     Path path;
 
-    public FileBucket() throws IOException {
-        this.path = Files.createTempFile("tmp", null);
+    public FileBucket() {
+        try {
+            this.path = Files.createTempFile("tmp", null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //add substitution logic
+        File newFile = path.toFile();
+        newFile.deleteOnExit();
+    }
 
+    public long getFileSize() {
+        try {
+            return Files.size(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void putEntry(Entry entry) {
+
+    }
+
+    public Entry getEntry() {
+        return null;
+    }
+
+    public void remove() {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
