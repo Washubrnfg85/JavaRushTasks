c2d7d3810ed1cae513fa4d2d7edaf783c540b2b8
ArchPJ <washubrnfg85@gmail.com>
2023-04-26 21:33:01 +0300

    Working on 3310, Shortener(9) done

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task33.task3310.strategy;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -12,17 +10,13 @@ public class FileBucket {
     public FileBucket() {
         try {
             this.path = Files.createTempFile("tmp", null);
-            File newFile = path.toFile();
-            if (newFile.isFile()) {
-                FileWriter fileWriter = new FileWriter(newFile, false);
-            } else {
+            path.toFile().deleteOnExit();
 
-            }
-            newFile.deleteOnExit();
+            Files.deleteIfExists(path);
+            Files.createFile(path);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //add substitution logic
 
     }
 
@@ -35,10 +29,23 @@ public class FileBucket {
     }
 
     public void putEntry(Entry entry) {
-
+        try {
+            ObjectOutputStream objectWriter = new ObjectOutputStream(Files.newOutputStream(path));
+            objectWriter.writeObject(entry);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Entry getEntry() {
+        if (getFileSize() > 0) {
+            try {
+                ObjectInputStream objectReader = new ObjectInputStream(Files.newInputStream(path));
+                return (Entry) objectReader.readObject();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
         return null;
     }
 
